e0d8bb12b01d20c87adeb36a53ba04fdd121a333
EstebanM95 <merlettiesteban@gmail.com>
2022-12-06 19:45:51 -0300

    el experimento AR

--- js/script.js
+++ js/script.js
@@ -1,51 +1,29 @@
-function colorChanger(){
-    let r = "#" + Math.floor(Math.random()*16777215).toString(16);
-    let g = "#" + Math.floor(Math.random()*16777215).toString(16);
-    let b = "#" + Math.floor(Math.random()*16777215).toString(16);
-    
+let intervalo;
 
-document.getElementById("fondo").style.backgroundImage = `linear-gradient(to bottom right, ${r}, ${g}, ${b})`;
-//document.getElementById("body").style.backgroundImage = `linear-gradient(to bottom right, ${r}, ${g}, ${b})`;
-console.log(r,g,b) ;
+function colorChanger() {
+  let r = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let g = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let b = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+  document.getElementById(
+    "fondo"
+  ).style.backgroundImage = `linear-gradient(to bottom right, ${r}, ${g}, ${b})`;
+  //document.getElementById("body").style.backgroundImage = `linear-gradient(to bottom right, ${r}, ${g}, ${b})`;
 }
 
+function autoChanger() {
+  var n = 0;
+  window.setInterval(function () { n++;if (n % 5 == 0) {
+      colorChanger();
+    }
+  }, 1000);
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+  colorChanger();
+  autoChanger();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", ()=>{
+  document.getElementById("colorChangerBtn").addEventListener("click", () => {
     colorChanger();
-document.getElementById("colorChangerBtn").addEventListener("click",()=>{
-    colorChanger();
-})
-
-
-})
+  });
+});
